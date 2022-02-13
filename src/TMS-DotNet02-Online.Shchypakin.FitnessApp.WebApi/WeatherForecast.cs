bce5205a108819b45b23fb6eb6e6e3a9769be517
s4ipakin <62203513+s4ipakin@users.noreply.github.com>
2022-02-13 16:40:31 +0300

    feat: add WebApi

---
+++ src/TMS-DotNet02-Online.Shchypakin.FitnessApp.WebApi/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TMS_DotNet02_Online.Shchypakin.FitnessApp.WebApi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
