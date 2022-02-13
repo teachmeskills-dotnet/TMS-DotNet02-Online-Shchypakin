f9efbda091d979fa471d654a55276741b7e6db62
s4ipakin <62203513+s4ipakin@users.noreply.github.com>
2022-02-13 17:17:27 +0300

    feat: add constants

---
+++ src/TMS-DotNet02-Online.Shchypakin.FitnessApp.Data/Constants/Schema.cs
@@ -0,0 +1,18 @@
+﻿namespace Masny.TimeTracker.Data.Constants
+{
+    /// <summary>
+    /// Schema constants.
+    /// </summary>
+    public static class Schema
+    {
+        /// <summary>
+        /// User.
+        /// </summary>
+        public const string Client = "client";
+
+        /// <summary>
+        /// Tracker.
+        /// </summary>
+        public const string Membership = "membership";
+    }
+}
---
+++ src/TMS-DotNet02-Online.Shchypakin.FitnessApp.Data/Constants/Table.cs
@@ -0,0 +1,30 @@
+﻿namespace Masny.TimeTracker.Data.Constants
+{
+    /// <summary>
+    /// Table constants.
+    /// </summary>
+    public static class Table
+    {
+        /// <summary>
+        /// User.
+        /// </summary>
+        public const string Clients = nameof(Clients);
+
+        /// <summary>
+        /// Project.
+        /// </summary>
+        public const string Memberships = nameof(Memberships);
+
+        /// <summary>
+        /// Record.
+        /// </summary>
+        public const string MembershipSizes = nameof(MembershipSizes);
+
+        /// <summary>
+        /// Goal.
+        /// </summary>
+        public const string MembershipTypes = nameof(MembershipTypes);
+
+        public const string MembershipHistoryRecords = nameof(MembershipHistoryRecords);
+    }
+}
